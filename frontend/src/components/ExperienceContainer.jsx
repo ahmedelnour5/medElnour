9ba6902da652f9ccf88b272e04813d2d4f4474c4
Ahmed Elnour <ahmedelnour@Ahmeds-Air.fios-router.home>
2023-12-03 03:19:35 -0500

    add api proxy to vite config, make experiences responsive

--- frontend/src/components/ExperienceContainer.jsx
+++ frontend/src/components/ExperienceContainer.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import cartLogo from "../assets/cartdotcom_logo.jpeg";
+import codeHouseLogo from "../assets/codeHouse_logo.jpeg";
+import vcu from "../assets/virginia_commonwealth_university_logo.jpeg";
 
-const ExperienceContainer = () => {
-  return <div>ExperienceContainer</div>;
+const ExperienceContainer = ({ experience }) => {
+  return (
+    <div className="bg-white shadow-md text-black rounded-lg p-8 relative md:w-[60%]">
+      <div className="flex flex-col items-center md:flex md:flex-row md:items-start">
+        <img
+          src={
+            experience.organization === "Code House Solutions"
+              ? codeHouseLogo
+              : experience.organization === "Cart.com"
+              ? cartLogo
+              : vcu
+          }
+          className="w-[150px] h-auto md:h-[100px] md:w-auto mb-4"
+        />
+        <div className="flex flex-col md:ml-16">
+          <h2 className="font-medium">{experience.organization}</h2>
+          <div className="flex justify-between">
+            <h2 className="text-sm md:text-lg md:font-semibold">
+              {experience.title}
+            </h2>
+            <span className="text-sm md:text-base text-gray-500">
+              {experience.dates}
+            </span>
+          </div>
+          <div className="text-sm md:text-base flex justify-between text-gray-500">
+            <span>{experience.position}</span>
+            <span>{experience.location}</span>
+          </div>
+          <ul className="list-inside list-disc pt-5 mt-2 text-gray-500 text-sm md:text-base space-y-1">
+            {experience.description.map((desc) => (
+              <li className="leading-5">{desc}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExperienceContainer;
--- frontend/src/pages/Experience.jsx
+++ frontend/src/pages/Experience.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ExperienceContainer from "../components/ExperienceContainer.jsx";
+import axios from "axios";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  const [experiences, setExperiences] = useState();
+  useEffect(() => {
+    const getExperiences = async () => {
+      const response = await axios.get("/api/Experience");
+      if (response) {
+        setExperiences(response.data);
+      }
+    };
+    getExperiences();
+  }, []);
+  return (
+    <div className="container mx-auto min-h-screen mt-4 pt-2">
+      <div className="space-y-7 flex flex-col justify-center items-center">
+        {experiences && experiences !== null
+          ? experiences.map((experience) => (
+              <ExperienceContainer
+                key={experience._id}
+                experience={experience}
+              />
+            ))
+          : null}
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
